91868c41417fc30e4949ac262e8c6a84e618510f
triabagus <triatop9@gmail.com>
2022-12-23 22:45:03 +0700

    Product section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,24 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
+let swiperProducts = new Swiper(".products__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        1024: {
+            spaceBetween: 72,
+        },
+    }
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
